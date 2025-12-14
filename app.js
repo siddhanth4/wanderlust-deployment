7bacb0f3367314a945efd34c654b2637352f260b
siddhanth4 <123siddhanth@gmail.com>
2025-12-14 14:16:15 +0530

    updated

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-// const dbUrl = process.env.ATLASDB_URL;
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 main()
    .then(() => {
       console.log("connected to db");
@@ -31,8 +31,8 @@ main()
    });
 
 async function main(){
-    // await mongoose.connect(dbUrl);
-        await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
+    // await mongoose.connect(MONGO_URL);
 
 }
 
@@ -44,8 +44,8 @@ app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
 const store = MongoStore.create({
-    //mongoUrl: dbUrl,
-     mongoUrl: MONGO_URL,
+    mongoUrl: dbUrl,
+    //  mongoUrl: MONGO_URL,
     crypto: {
         secret: process.env.SECRET,
     },
